7b8bec7b68eeb3acf378b175f1fb127da2679eeb
Alain Picard <Dr.Alain.Picard@gmail.com>
2017-03-16 14:08:59 +1100

    slide 2 code

--- src/my_compojure_app/module_a.clj
+++ src/my_compojure_app/module_a.clj
@@ -1,10 +1,17 @@
+;;; Imagine these 3 chunks are all in their
+;;; own files, perhaps being developed by different
+;;; developers...
+
 (ns my-compojure-app.module-a)
 (compojure.core/defroutes module-routes
   (compojure.core/GET "/foo" [] (constantly {:status 200 :body "module A"})))
 
+
 (ns my-compojure-app.module-b)
 (compojure.core/defroutes module-routes
-  (compojure.core/GET "/bar" [] (constantly {:status 200 :body "module B"})))
+  (compojure.core/GET "/bar" [] (constantly {:status 200 :body "module B"}))
+  ;; Bob adds this route
+  (compojure.core/GET "/users" [] (constantly {:status 200 :body "module B Users"})))
 
 
 (ns my-compojure-app.module-c)
