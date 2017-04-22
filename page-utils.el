eca2b84896c2d3a386572de4e77498d59dc0703f
Alain Picard <Dr.Alain.Picard@gmail.com>
2017-04-22 11:16:57 +1000

    Add function to swap between http and presentation with f7

--- page-utils.el
+++ page-utils.el
@@ -10,7 +10,20 @@
   (backward-page 2)
   (narrow-to-page))
 
+(defun ap-toggle-prezzo ()
+  (interactive)
+  (let ((routes (get-buffer "example_routes.clj"))
+	(examples (get-buffer "examples.http"))
+	(this (current-buffer)))
+    (switch-to-buffer (if (eql this routes)
+			  examples
+			routes))
+    (delete-other-windows)))
+
 
 
 (define-key clojure-mode-map (kbd "C-x [") 'prev-hidden-page)
 (define-key clojure-mode-map (kbd "C-x ]") 'next-hidden-page)
+(define-key clojure-mode-map (kbd "<f5>") 'prev-hidden-page)
+(define-key clojure-mode-map (kbd "<f6>") 'next-hidden-page)
+(global-set-key (kbd "<f7>") 'ap-toggle-prezzo)
